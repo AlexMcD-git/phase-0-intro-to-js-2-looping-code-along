09430282322a8b744fa5a35a85113c0d2de6496a
Alex McDaniel <alexpmcdaniel@gmail.com>
2022-04-05 17:01:59 -0400

    commit tree ecc52f6b3d76b3867b08fce3c5edff6cef49e3fc

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const names= []
+
+function writeCards(names, event){
+    let newArray= []
+    for(let i =  0; i < names.length; i++){
+        newArray[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+    }
+    return newArray
+}
+function countDown(number){
+    while (number>=0){
+        console.log(number)
+        number--
+    }
+}
